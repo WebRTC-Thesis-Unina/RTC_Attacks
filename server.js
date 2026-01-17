02ad5b99e17d6307f40eeb4717c0f9bee4a6243c
martinaborgstrom <martinaborgstrom02@gmail.com>
2026-01-17 17:41:51 +0100

    NodeJS server

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const http = require('http');
 const mongoose = require('mongoose');
-const { startInstance, stopInstance } = require('./ec2/ec2');
-const os = require('os');
-const SSH2Promise  = require("ssh2-promise");
-const fs = require("fs");
+const {spawn, exec} = require("child_process")
 
 const app = express();
 app.use(express.static("public"))
@@ -17,8 +14,8 @@ const scenarioSchema = new mongoose.Schema({
     id: Number,
     name: String,
     description: String,
-    images: Array,
-    steps: String
+    steps: String,
+    containers: Array
 });
 
 const Scenario = mongoose.model("scenario", scenarioSchema);
@@ -34,9 +31,9 @@ app.get("/scenarios",async (req, res)=>{
         const names = scenarios.map(s => s.name);
         const ids = scenarios.map(s => s.id);
 
-        res.json({names: names, ids: ids});    
-    } catch (error) {
-        console.error(error);
+        return res.json({names: names, ids: ids});    
+    } catch(err){
+        return res.status(500).json({ok: false, err: err.message});
     }
 })
 
@@ -49,120 +46,195 @@ app.get("/scenario/:id", async(req, res) => {
             return res.status(404).json({ error: "Scenario not found" });
         }
 
-        res.json({name: scenario.name, description: scenario.description, steps: scenario.steps});
-    } catch (error) {
-        console.error(error);
-    }
-})
-
-function getVMIP() {
-  const interfaces = os.networkInterfaces();
-  for (const name of Object.keys(interfaces)) {
-    for (const iface of interfaces[name]) {
-      if (iface.family === 'IPv4' && !iface.internal) {
-        return iface.address;
-      }
-    }
-  }
-  return 'IP not found';
-}
-
-app.get("/make-start/:ip/:hostname/:key/:id", async(req, res) => {
-    let ip = req.params.ip;
-    let hostname = req.params.hostname;
-    const key = req.params.key;
-    const id = req.params.id;
-
-    if(hostname === "none"){
-        hostname = os.hostname();
-    }
-    if(ip === "none"){
-        ip = getVMIP()
-    }
-
-    try {
-        global.ssh = new SSH2Promise({
-            host: ip,
-            username: hostname,
-            privateKey: fs.readFileSync("./ttyd/"+key)
-        });
-        
-        
-        const ssh = global.ssh;
-        await ssh.connect();
-        
-        await ssh.exec(`
-            cd ~/RTC_Attacks && \
-            make start SSH_IP=${ip} SSH_KEY=${key} SSH_HOSTNAME=${hostname} && \
-            cd ~/RTC_Attacks/public/labs/${id} && \
-            make start
-        `);
-
-        res.json({ ok: true});
+        return res.json({name: scenario.name, description: scenario.description, steps: scenario.steps});
     } catch(err){
-        console.error(err);
-        res.status(500).json({ok: false, err: err.message});
+        return res.status(500).json({ok: false, err: err.message});
     }
 })
 
-app.get("/make-stop/:id", async(req, res) => {
-    const id = req.params.id;
+app.post("/make-start", async(req, res) => {
+    const folder = req.body.folder;
 
-    try {
-        const ssh = global.ssh;
+    res.setHeader("Content-Type", "text/plain; charset=utf-8");
+    res.setHeader("Transfer-Encoding", "chunked");
     
-        await ssh.connect();
-    
-        await ssh.exec(`cd ~/RTC_Attacks/public/labs/${id} && make stop`);
-        res.json({ ok: true});
-    } catch(err){
-        console.error(err);
-        res.status(500).json({ok: false, err: err.message});
-    }
+    let buildCmd = `
+        make start && cd ${__dirname}/public/labs/${folder} && make start
+    `;
+
+    const stream = spawn(buildCmd, { shell: true });
+
+    stream.stdout.on("data", (chunk) => {
+        res.write(chunk.toString());
+    });
+
+    stream.stderr.on("data", (chunk) => {
+        res.write(chunk.toString());
+    });
+
+    stream.on("close", () => {
+        res.end();
+    });
 })
 
-app.get("/make-stop-ttyd", async(req, res) => {
-    
-    try {
-        const ssh = global.ssh;
-        await ssh.connect();
+app.post("/make-stop", async(req, res) => {
+    const folder = req.body.folder;
+        
+    let buildCmd = `cd ${__dirname}/public/labs/${folder} && make stop`;
 
-        await ssh.exec("cd ~/RTC_Attacks && make stop")
-        res.json({ ok: true }); 
-    } catch (error) {
-        res.status(500).json({ ok: false, message: "Problem in SSH connection" });
-    }
-    
-})
+    res.setHeader("Content-Type", "text/plain; charset=utf-8");
+    res.setHeader("Transfer-Encoding", "chunked");
 
-app.get("/start-ec2", async(req, res) => {
-    try {
-        const info = await startInstance();
-        res.json({ ok: true, ip: info.publicIp, region: info.region });
-    } catch (error) {
-        console.error("Error: ", error);
-    }
-})
+    const stream = spawn(buildCmd, { shell: true });
 
-app.get("/stop-ec2", async(req, res) => {
-    try {
-        const info = await stopInstance();
-        res.json({ ok: true, ip: info.publicIp });
-    } catch (error) {
-        console.error("Error: ", error);
-    }
+    stream.stdout.on("data", (chunk) => {
+        res.write(chunk.toString());
+    });
+
+    stream.stderr.on("data", (chunk) => {
+        res.write(chunk.toString());
+    });
+
+    stream.on("close", () => {
+        res.end();
+    });
 })
 
 app.get("/search", async(req, res) => {
     const name = req.query.name;
     try {
-        // Ricerca parziale
-        const scenarios = await Scenario.find({ name:{ $regex: name, $options: "i" }});
+        const scenarios = await Scenario.find({ name:{ $regex: name, $options: "i" }}).sort({id: 1});;
         if (scenarios.length === 0) {
             return res.status(404).json({ error: "Scenario not found" });
         }
-        res.json(scenarios)
-    } catch (error){
-        console.error("Error:", err);
+        return res.json(scenarios)
+    } catch(err){
+        return res.status(500).json({ok: false, err: err.message});
     }
 })
+
+app.post("/build-all", async(req, res) => {
+    const folder = req.body.folder;
+    const elements = req.body.elements;
+    
+    const container = elements.replaceAll(","," ")
+
+    res.setHeader("Content-Type", "text/plain; charset=utf-8");
+    res.setHeader("Transfer-Encoding", "chunked");
+
+    let buildCmd;
+
+    if (container.includes("ttyd") && container.includes("collector")){
+        const [ttyd, collector] = container.split(" ").slice(-2);
+        const containerNew = container.replaceAll(/ttyd|collector/g, "").trim();
+        
+        buildCmd = `make build SERVICE="${ttyd} ${collector}" &&
+            cd ${__dirname}/public/labs/${folder} && make build SERVICE="${containerNew}"`
+    
+    } else if (container.includes("ttyd") || container.includes("collector")){
+        const [ttyd_collector] = container.split(" ").slice(-1);
+        const containerNew = container.replaceAll(/ttyd|collector/g, "").trim();
+    
+        buildCmd = `make build SERVICE="${ttyd_collector}" &&
+            cd ${__dirname}/public/labs/${folder} && make build SERVICE="${containerNew}"`
+    
+    } else {
+        buildCmd = `cd ${__dirname}/public/labs/${folder} && make build SERVICE="${container}"`
+    }
+    
+    const stream = spawn(buildCmd, { shell: true });
+
+    stream.stdout.on("data", (chunk) => {
+        res.write(chunk.toString());
+    });
+
+    stream.stderr.on("data", (chunk) => {
+        res.write(chunk.toString());
+    });
+
+    stream.on("close", () => {
+        res.end();
+    });
+})
+
+app.post("/build", async(req, res) => {
+    const image = req.body.image;
+    const folder = req.body.folder;
+
+    res.setHeader("Content-Type", "text/plain; charset=utf-8");
+    res.setHeader("Transfer-Encoding", "chunked");
+
+    let buildCmd;
+
+    if(image === "ttyd" || image === "collector"){
+        buildCmd = `make build SERVICE="${image}"`
+    } else {
+        buildCmd = `cd ${__dirname}/public/labs/${folder} && make build SERVICE="${image}"`
+    }
+    
+    const stream = spawn(buildCmd, { shell: true });
+
+    stream.stdout.on("data", (chunk) => {
+        res.write(chunk.toString());
+    });
+
+    stream.stderr.on("data", (chunk) => {
+        res.write(chunk.toString());
+    });
+
+    stream.on("close", () => {
+        res.end();
+    });
+})
+
+app.post("/images-to-build", async(req, res) =>{
+    const id = req.body.id;
+
+    try {
+        const scenario = await Scenario.findOne({id: id})
+        const containers = [...scenario.containers, "ttyd", "collector"];
+
+        if (!scenario) {
+            return res.status(404).json({ error: "Scenario not found" });
+        }
+
+        exec(`docker images --format "{{.Repository}}"`, (err, stdout, stderr) => {
+            if (err) {
+                return res.status(500).json({ ok: false, err: err.message });
+            }
+
+            const dockerImages = stdout
+                .split("\n")
+                .map(i => i.trim())
+                .filter(Boolean);
+
+            const dockerSet = new Set(dockerImages);
+
+            const containersToSend = containers.filter(img => !dockerSet.has(img));
+
+            res.json({ containers: containersToSend });
+        });
+
+    } catch (err) {
+        return res.status(500).json({ok: false, err: err.message});
+    }
+})
+
+async function shutdown() {
+  try {
+    console.log("Stopping containers...");
+
+    exec("make stop", (err) => {
+      if (err) {
+        console.error("Error:", err);
+      } 
+      process.exit(0);
+    });
+
+  } catch (err) {
+    console.error("Error:", err);
+    process.exit(1);
+  }
+};
+
+process.on("SIGINT", shutdown);
